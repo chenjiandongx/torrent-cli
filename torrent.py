577a1ed5a7ad5645a73706d3f8216cda2707538a
chenjiandongx <chenjiandongx@qq.com>
2017-11-16 00:01:30 +0800

    代码初整理

--- torrent.py
+++ torrent.py
@@ -1,32 +1,25 @@
 import requests
-import re
 from bs4 import BeautifulSoup
 
 
-domain = "http://bt2.bt87.cc"
-#
-# url = "http://bt2.bt87.cc/index.php?r=files/index&kw="
-# resp = requests.get(url).text
-#
-# url_pattern = re.compile(r'(/index.php\?r=files/view&infohash=\S+)"')
-# urls = [domain + u for u in re.findall(url_pattern, resp)]
-#
-#
-# resp = requests.get(url).text
-# magnet_pattern = re.compile(r'(magnet:\?\S+)"')
-# magnets = re.findall(magnet_pattern, resp)
+def spider(kw=None, page=1, sort_by=None):
 
-# r = requests.get("http://bt2.bt87.cc/index.php?r=files/view&infohash=4952a2260099b88c1e84e33664c5df58eb542b1b").text
-# magnet = BeautifulSoup(r, 'lxml').find("h4", id="magnet").text[8:]
-# magnet_name = BeautifulSoup(r, 'lxml').find("h3").text
-# magnet_date, magnet_size = (str(BeautifulSoup(r, 'lxml').find("h4").text).split(maxsplit=1))
-#
-# torrent = (magnet, magnet_name, magnet_date, magnet_size)
-# print(torrent)
+    domain = "http://bt2.bt87.cc"
+    url = domain + "/index.php?r=files%2Findex&kw={kw}".format(kw=kw)
+    resp = requests.get(url).text
+    bs = BeautifulSoup(resp, "lxml").find("ul",class_="row list-group").find_all("li")
+    urls = [domain + b.find("a")["href"] for b in bs]
 
-# urls = []
-url = domain + "/index.php?r=files%2Findex&kw={kw}"
-resp = requests.get(url).text
-bs = BeautifulSoup(resp, "lxml").find("ul", class_="row list-group").find_all("li")
+    magnets = []
+    for url in urls:
+        resp = requests.get(url).text
+        magnet = BeautifulSoup(resp, 'lxml').find("h4", id="magnet").text[8:]
+        magnet_name = BeautifulSoup(resp, 'lxml').find("h3").text
+        magnet_date, magnet_size = (
+            str(BeautifulSoup(resp, 'lxml').find("h4").text).split(maxsplit=1))
 
-urls = [domain + b.find("a")["href"] for b in bs]
+        torrent = (magnet, magnet_name, magnet_date, magnet_size)
+        magnets.append(torrent)
+
+    from pprint import pprint
+    pprint(magnets)
