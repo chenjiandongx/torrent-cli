34d49c80f548aad8945366b15379f6971bc190d2
chenjiandongx <chenjiandongx@qq.com>
2017-11-16 17:34:51 +0800

    准备发布库

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
-from torrent import __version__
 
 setup(
     author="chenjiandongx",
     author_email="chenjiandongx@qq.com",
     name="torrent-cli",
-    version=__version__,
+    version="0.0.1",
     license="MIT",
     url = "https://github.com/chenjiandongx/torrent-cli",
     py_modules=['torrent',],
+    install_requires=['bs4', 'requests', 'lxml'],
     description="Magnets-Getter CLI Tools",
     entry_points={
         'console_scripts':['torrent-cli=torrent:command_line_runner']
